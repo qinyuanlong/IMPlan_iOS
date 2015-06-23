50d68563c7605a834bc64c83ab8420af79f752a9
qinyuanlong <qinyuanlong@gmail.com>
2015-06-23 11:30:42 +0800

    init project with cocoapods

---
+++ source/IMPlan_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMPlan_iOS
+//
+//  Created by 覃远龙 on 15/6/23.
+//  Copyright (c) 2015年 qinyuanlong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ source/bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  IMPlan_iOS
+//
+//  Created by 覃远龙 on 15/6/23.
+//  Copyright (c) 2015年 qinyuanlong. All rights reserved.
+//
+
+#ifndef IMPlan_iOS_bridge_h
+#define IMPlan_iOS_bridge_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#endif
